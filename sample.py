fff6777b893dff6d0ac1007a11ddd9f695c4a6ac
Aljohn Motas <42311295+alojohn@users.noreply.github.com>
2020-05-09 15:05:09 +0800

    part 1

--- sample.py
+++ sample.py
@@ -1,4 +1,25 @@
 print("Aljohn")
 
-def al():
-    print("Pogi")
+def adder():
+    return ("congrtualations for finnishing python part 1")
+
+List = ["apple","banana","cherry"]
+Tuple = ("apple","banana","cherry")
+Set = {"apple","banana","cherry"}
+
+List[0] = "banana"
+Set.remove("banana")
+Set.update(['car'])
+Set.add("Theo")
+print(List[0])
+print(Tuple[0])
+print(Set)
+
+print(adder())
+
+i = 1 
+while i < 5:
+    print(i)
+    if i == 3:
+     break
+    i+=1
